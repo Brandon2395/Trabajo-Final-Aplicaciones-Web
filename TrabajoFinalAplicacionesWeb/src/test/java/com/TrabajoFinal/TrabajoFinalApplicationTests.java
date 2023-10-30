96b0d36f54797e1c18db6e62943fa3b8dca72766
Brandon2395 <acostabrandon2395@gmail.com>
2023-10-30 15:17:28 -0600

    Feat: Código Inicial
    Código Inicial de Netbeans

---
+++ TrabajoFinalAplicacionesWeb/src/test/java/com/TrabajoFinal/TrabajoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrabajoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
