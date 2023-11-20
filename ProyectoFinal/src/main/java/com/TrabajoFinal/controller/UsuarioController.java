5e14d83823a311921512f1320908a684d5d4d91a
Brandon2395 <acostabrandon2395@gmail.com>
2023-11-20 14:39:51 -0600

    Feat: Codigo
    Se agrego el uso de páginas estaticas al código de manera funcional

---
+++ ProyectoFinal/src/main/java/com/TrabajoFinal/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.TrabajoFinal.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/usuario")
+public class UsuarioController {
+    
+    
+   @GetMapping("listado")
+    public String getIngresoUsuario() {
+        return "/usuario/listado"; // Nombre del archivo HTML sin extensión
+    }
+}
