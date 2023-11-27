4bc0f8404219b4583e88061ca74abfcd15928186
Brandon2395 <acostabrandon2395@gmail.com>
2023-11-27 13:43:44 -0600

    Feat: Ecommerce
    Se agregó la parte de los dos compañeros que elaboraron las pestañas Ecommerce, Desarollo de Software y servicios

---
+++ ProyectoFinal/src/main/java/com/TrabajoFinal/controller/ServiciosController.java
@@ -0,0 +1,29 @@
+package com.TrabajoFinal.controller;
+
+
+import com.TrabajoFinal.domain.Contacto;
+import com.TrabajoFinal.service.CorreoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@Slf4j
+@RequestMapping("/servicios")
+public class ServiciosController {
+
+    @GetMapping("listado")
+    public String getContacto() {
+        return "/servicios/listado";
+    }
+}
+    
